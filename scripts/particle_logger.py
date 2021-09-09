3cea29ecc8ec6458e68505d77c0c825c7fbf9421
Alex Bencz <abencz@gmail.com>
2021-09-09 23:17:00 +0100

    Logger and web UI used in first hatch
    particle_logger.py polls the particle API for temperature/RH and logs
    data to .csv files on disk. quail_maker_web.py is a Flask app that reads
    the logged CSV data and presents it as a simple web page. It also allows
    all data to be downloaded in a combines .csv file.

---
+++ scripts/particle_logger.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+import argparse
+import csv
+import requests
+import time
+from datetime import datetime, timezone
+
+
+# Example curl command from Particle documentation
+# curl "https://api.particle.io/v1/devices/0123456789abcdef01234567/temperature?access_token=1234"
+
+
+def get_variable(device_id, access_token, variable, retries=5):
+    url = f"https://api.particle.io/v1/devices/{device_id}/{variable}?access_token={access_token}"
+    
+    while retries > 0:
+        try:
+            r = requests.get(url)
+            r.raise_for_status()
+        except requests.exceptions.HTTPError:
+            print(f"Failed to fetch variable '{variable}' at {datetime.now().isoformat()} due to {r.status_code}")
+        except Exception as e:
+            print(f"Failed to fetch variable '{variable}' at {datetime.now().isoformat()} due to {e}")
+        else:
+            result = r.json()
+            var_value = float(result["result"])
+            utc = datetime.fromisoformat(result["coreInfo"]["last_heard"][:-1])
+            local = utc.replace(tzinfo=timezone.utc).astimezone(tz=None)
+            return (var_value, local)
+
+        retries -= 1
+        time.sleep(3)
+
+    return None
+
+
+def log_data(output_file, device_id, access_token, log_delay=3600):
+    fieldnames = ["timestamp", "temperature", "rh"]
+    writer = csv.DictWriter(output_file, fieldnames=fieldnames)
+    writer.writeheader()
+    last_timestamp = datetime(2000, 1, 1)
+
+    while True:
+        try:
+            temp, timestamp = get_variable(device_id, access_token, "temperature")
+            rh, timestamp = get_variable(device_id, access_token, "rh")
+        except TypeError:
+            time.sleep(log_delay)
+            continue
+
+        if last_timestamp == timestamp:
+            print(f"Timestamp unchanged from last API call")
+        else:
+            last_timestamp = timestamp
+
+            writer.writerow({
+                "timestamp": timestamp.isoformat(),
+                "temperature": temp,
+                "rh": rh
+            })
+            output_file.flush()
+            print(f"[{timestamp.isoformat()}, {temp}, {rh}]")
+        time.sleep(log_delay)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file-prefix", type=str, default="quail_maker")
+    parser.add_argument("-d", "--log-delay", type=int, default=300)
+
+    return parser.parse_args()
+
+
+def main():
+    device_id = "<tbd>"
+    access_token = "<tbd>"
+    args = parse_args()
+
+    filename = f"{args.file_prefix}-{datetime.now().timestamp()}.csv"
+
+    with open(filename, "w", newline="") as csvfile:
+        log_data(csvfile, device_id, access_token, args.log_delay)
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/quail_maker_web.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import csv
+from datetime import datetime, timedelta, timezone
+from flask import Flask, send_file
+from glob import glob
+from tempfile import NamedTemporaryFile
+
+
+def get_csv_files(directory):
+    path = f"{directory}/*.csv"
+    files = glob(path)
+    return sorted(files)
+
+
+def gather_data(directory="./"):
+    files = get_csv_files(directory)
+
+    data = []
+
+    edt = timezone(timedelta(hours=-4))
+    for f in files:
+        with open(f, newline="") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                row["timestamp"] = datetime.fromisoformat(row["timestamp"]).astimezone(tz=edt)
+                row["temperature"] = float(row["temperature"])
+                row["rh"] = float(row["rh"])
+                data.append(row)
+
+    return data
+
+
+def row_to_html(row):
+    return f"""
+    <tr>
+        <td>{row["timestamp"].isoformat()}</td>
+        <td><center>{row["temperature"]:.1f} \xb0C</center></td>
+        <td><center>{row["rh"]:.1f} %</center></td>
+    </tr>
+    """
+
+def populate_downloadable_csv(output_file, data):
+    fieldnames = ["Timestamp", "Tempincture", "Humidity"]
+    writer = csv.DictWriter(output_file, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for datum in data:
+        writer.writerow({
+            "Timestamp": datum["timestamp"].strftime("%Y-%m-%d %H:%M:%S"),
+            "Tempincture": f"{datum['temperature']:.1f}",
+            "Humidity": f"{datum['rh']:.1f}"
+        })
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def quail_log_dump():
+    data = gather_data()
+    html_data = "\n".join(row_to_html(row) for row in reversed(data))
+    return f"""
+    <h1>Hello, Quail!</h1>
+    <a href="/download">Download</a>
+    <table>
+        <tr>
+            <td><h3>Timestamp</h3></td>
+            <td><h3>Tempincture</h3></td>
+            <td><h3>Humidity</h3></td>
+        </tr>
+        {html_data}
+    </table>
+    """
+
+@app.route("/download")
+def download_quail_log():
+    data = gather_data()
+    with NamedTemporaryFile(mode="w", newline="", prefix="quail_maker_", suffix=".csv") as output_csvfile:
+        populate_downloadable_csv(output_csvfile, data)
+        output_csvfile.flush()
+        return send_file(output_csvfile.name, as_attachment=True)
